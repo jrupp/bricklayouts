f4e65e2954096555417909f83d37e82e170fe486
Justin Rupp <dither@gmail.com>
2025-03-16 12:27:24 -0400

    Fix flickering graphics on retina displays

--- src/index.js
+++ src/index.js
@@ -4,11 +4,21 @@ import { Application, Assets } from './pixi.mjs';
 const canvasContainer = document.getElementById('canvasContainer');
 document.body.style.setProperty('--canvas-bg', '#93bee2');
 const app = new Application();
-await app.init({ background: '#93bee2', resizeTo: canvasContainer });
+await app.init({ background: '#93bee2', resizeTo: canvasContainer, resolution: window.devicePixelRatio ?? 1 });
 canvasContainer.appendChild(app.canvas);
 await Assets.init({ basePath: '../img/', manifest: "../data/manifest.json" });
 await Assets.loadBundle('track');
 window.app = app;
 window.assets = Assets;
+function listenOnDevicePixelRatio() {
+  function onChange() {
+    window.app.renderer.resolution = window.devicePixelRatio;
+    listenOnDevicePixelRatio();
+  }
+  matchMedia(
+    `(resolution: ${window.devicePixelRatio}dppx)`
+  ).addEventListener("change", onChange, { once: true });
+}
+listenOnDevicePixelRatio();
 const layoutController = new LayoutController(app);
 await layoutController.init();
